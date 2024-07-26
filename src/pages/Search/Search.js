269adcbfa89ff0ab19d0058718f608d691d1f0ce
navduhan <duhan27dec@gmail.com>
2024-07-26 15:39:49 -0500

    added more content blast

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { Divider, Button, Radio} from "antd";
+import { Slider, Divider, Button, Radio } from "antd";
 import { Form } from "react-bootstrap";
 import '../../scss/components/buttons.scss';
 import '../../scss/style.scss'
 import FileInput from '../../components/FileInput/Fileinput';
 import "./search.scss";
+import test from './test.gif'
+import { fetchBlast } from "./fetchBlast";
 
 export default class Search extends React.Component {
   constructor(props) {
@@ -12,11 +14,18 @@ export default class Search extends React.Component {
     this.state = {
       seqType: "nucl",
       segmentType: "M2",
-      strainType:'All',
+      strainType: 'All',
       isOpen: false,
-      atype:'phylo',
-      msatool:'mafft',
+      atype: 'blast',
+      msatool: 'mafft',
       phylomethod: 'ml',
+      bprog: 'blastn',
+      word: '20',
+      target: '5',
+      evalue: '0.01',
+      filename: '',
+      seqtxt: ''
+
     };
     this.seqHandler = this.seqHandler.bind(this);
     this.segmentHandler = this.segmentHandler.bind(this);
@@ -24,9 +33,14 @@ export default class Search extends React.Component {
     this.analysisTypeHandler = this.analysisTypeHandler.bind(this);
     this.getPhylogeny = this.getPhylogeny.bind(this);
     this.fileSelected = this.fileSelected.bind(this);
-    this.handleGeneChange = this.handleGeneChange.bind(this);
+    this.handleSeqChange = this.handleSeqChange.bind(this);
     this.msaToolHandler = this.msaToolHandler.bind(this);
     this.phyloMethodHandler = this.phyloMethodHandler.bind(this);
+    this.blastToolHandler = this.blastToolHandler.bind(this);
+    this.getWord = this.getWord.bind(this);
+    this.getTarget = this.getTarget.bind(this);
+    this.getEvalue = this.getEvalue.bind(this);
+    this.runBlast = this.runBlast.bind(this);
   }
 
   openModel = () => this.setState({ isOpen: true });
@@ -48,50 +62,129 @@ export default class Search extends React.Component {
     });
   }
 
-  analysisTypeHandler(e){
+  analysisTypeHandler(e) {
     this.setState({
       atype: e.target.value,
     });
   }
 
-  msaToolHandler(e){
+  msaToolHandler(e) {
     this.setState({
       msatool: e.target.value,
     });
   }
 
-  phyloMethodHandler(e){
+  phyloMethodHandler(e) {
     this.setState({
       phylomethod: e.target.value,
     });
   }
 
+  blastToolHandler(e) {
+    this.setState({
+      bprog: e.target.value,
+    });
+  }
+
+  getWord = (value) => {
+
+    this.setState({
+      word: parseInt(value)
+    }, () => {
+
+    });
+  }
+
+  getTarget = (value) => {
+
+    this.setState({
+      target: parseInt(value)
+    }, () => {
+
+    });
+  }
+
+  getEvalue(event) {
+    this.setState({
+      evalue: event.target.value
+    });
+  }
 
   fileSelected(fileText) {
-    const protein = fileText.trim().split("\n");
-    this.setState({ genes: protein });
-
+    this.setState({
+      filename: fileText.target.files[0], seqtxt: ''
+    });
   }
-  handleGeneChange(e) {
-    const input = e.target.value;
+  handleSeqChange(e) {
 
-    // Split the input based on commas, new lines, and tabs
-    const genesArray = input
-      .split(/,|\n|\t/)
-      .map((gene) => gene.trim()) // Trim each gene
-      .filter((gene) => gene); // Remove empty entries
-
-    const genesString = genesArray.join(',')
-
-
-
-    this.setState({ genes: genesString });
+    this.setState({
+      seqtxt: e.target.value, filename: ''
+    });
   }
-  getPhylogeny (){}
+  getPhylogeny() { }
+
+  runBlast() {
+    this.openModel();
+    const data = new FormData()
+    data.append('file', this.state.filename)
+    data.append('genome', this.state.segmentType)
+    data.append('seqtype', this.state.seqType)
+    data.append('seqtxt', this.state.seqtxt)
+    data.append('word', this.state.word)
+    data.append('target', this.state.target)
+    data.append('evalue', this.state.evalue)
+    data.append('program', this.state.bprog)
+
+    // console.log(data.values())
+
+    for (var pair of data.entries()) {
+      console.log(pair[0]+ ', ' + pair[1]);
+      }
+    
+    // fetchBlast(data)
+    //   .then(res => { // then print response status
+    //     console.log(res)
+    //     this.closeModel();
+    //     window.open("/ranchsatdb/blastresults", "_blank");
+    //     localStorage.setItem('resultb', JSON.stringify(res))
+    //   })
+  }
+
 
   render() {
-   
-    let geneSample = 'OAJ02622, OAI99867, OAJ05030, OAI99147';
+
+    let geneSample = `>AY750052.1 Avian orthoreovirus clone 138 major outer capsid protein muB (M2) gene, complete cds
+GCTTTTTCAGTGCCAGTCTTTCTCACAAGATGGGTAACGCGACGTCTGTTGTGCAGAACTTTAATATCCA
+AGGTGATGGTAATCATTTTGCTCCATCTGCTGAGACTACCTCATCCGCGGTGCCGTCATTATCTCTGAAT
+CCCGGACTGTTAAATCCAGGTGGTAAGGCGTGGGTCCTGATCGATCCATCTCTAAACGCTTCGGATCCTT
+CATCACTACGTTTGATGACATCGGCTGATCTATCAACACTTTCTCAATCCGCTACTAGCAACTCGACCGG
+ATTTCTTCCAACTTCTGGTATGTATGCCATCGCCACTAAAGAGACGTTAAGTGTAATAACCGAGCACGCA
+ATTTCTCAGTTTGATAAGTTGCAAATGGCTTGTGAGTTGGACCGCGATTATCTGGACGCTAGAGGTGTTT
+CTCCCGAGTCTGTGAACATTCATAATTATATAGTCTATGTTGATTGCTTTGTAGGTGTATCCGCAAGGCA
+AGCTGCGTCGAATTTCAAGCAGCACGTGCCAGTCATTACTAAATCTCGTATGACACAATTCATGACGTCC
+GCGCAAAATATGTTACAGGTGCTCGGACCCTGGGAACGTGACGTTCGTGAGTTACTCACTATTCTGCCCA
+CTTCTACCACCGCTGGTAAGATCACATGTGACATGAAGTCTGTTGTCGCTTTTATCGATGATCAGCTCTC
+TGATACCAGTTTGTGCCGTCTGTACCCGGACTGTGCTGCTGCGGCGGTGGCTAGACGTAATGGTGGCATT
+CGGTGGAAGACACCTGACACCGATGAGGCTCCTTCGCTCGCAACTAATGATATTGCTGCCTCAACTATGG
+GTACGCTTGCGAATACTACACCCCTGGCCGAGAAGTCGAATTCAGGTGAGGAGTCAATGCGCTTGGTGAG
+TGACGTTGGTGTAGACATCGTGTGTTCTCGTGGCCCTATCAGCTCTTCGGTTTGGTCACGCACGGTTGAA
+CCCAAATCGTATAATATTAGAACTCTTCGTGTCGAAGAAGCGCTTTGGTTGCGTGAGTGTCAAGCGACTA
+CTGGCTTCGATGTACAGTACACGCTGCCTGACCAGACTACACATAAGCATTTCTGGCTTCAGAAGGGGTC
+AGTTGTCATCAATCTTGAGCAAACGGGTAGTATGATGTTTGATGTGAACATAGCGGGTAAAGATTATAAG
+AAGGGCACTTTTAATCCCGATAATCGTAAACTGGTCCTTTTGGTTATGCAGTCAAAAATCCCTTTCGAAT
+CTTGGACTGTCGCTTCTCAGATTACTGGTATCGCCCAAGTAGCTGAAGTCACTGTGCATGCTGCTGACAG
+TTCGACTCTTAATCAAAAGATAATCGGCGAGACTTCTCTGTCTTATTTGTTTGAGAGAGAAACAGTGACC
+ACATCAAATACTGAAGTCAATACGTATCTACTATGCACTTGGCAACTTGATGAAAGCCAAAGCGATGACG
+CTAATGCCTGGCCAGATGCATGGGATGGGATTACGACATTGACACCGCTCACTTCCGGTACCGTAACTAT
+TAAGGGGACTTCGGTGGATTCTGTTGTGCCATCCGATTTAGTTGGCGCTTATACACCTGAGGCTTTGGCC
+GCCGCGCTTCCTAATGACGCTGGGTTAATCTTGGCTAATAAGGCAACTAAATTGGCCGACGCTATCCAGA
+AAGAGGATGATTCCGTGATTGATGAGTCTTCTCCCTTTAGCACTCCCATTCAAGGTGTTCTTGCTGTTCA
+ACAACTCGATACCGTTGGGACGCGTGGTACACGTGTACTCCAGCCTCCGTCCATTCTGAAACGCATCGCC
+TCACGAGCTCTTCACATGTTTCTTGGTGATCCAAAGTCTATTTTGAAACAGGCGACGCCCGTACTAAGGG
+ACCCTGATGTCTGGACCGGTTTTGTTCAAGGTGTTAGAGATGGTATCCGGACTAAGTCGCTATCCGCGGG
+GGTACGGTCGGTGTATAATAACGTTACCGCCACGCAATCTGTGCAAACGTGGAAGCAGGGATTTCTGACG
+AAAATACAGACGTTGTTCAAGCCATCGTGAGGTGCTAAGGCCTCTCTCTGCGGCGGGTCGGTGGGCACGT
+CGTGGTGACGCTGAATGCACGGGGAGGTGACGCTCCCTGGATTGGCACGTTATTCATC`;
     return (
       <div className="container">
         <div className="row flex-lg-row justify-content-center mt-2">
@@ -109,34 +202,11 @@ export default class Search extends React.Component {
               </Radio>
             </Radio.Group>
           </div>
-          <div className="col-md-9 my-3">
-            <Radio.Group name="radiogroup" defaultValue={"TRV"}>
-              <h5>Select Variant Type</h5>
-              <Radio value="TRV" onClick={this.strainHandler}>
-               Turkey reovirus  (TRV)
-              </Radio>
-              <Radio value="TERV" onClick={this.strainHandler}>
-               Turkey enteric reovirus (TERV)
-              </Radio>
-              <Radio value="TARV" onClick={this.strainHandler}>
-               Turkey arthritis reovirus (TARV)
-              </Radio>
-              <Radio value="THRV" onClick={this.strainHandler}>
-                Turkey hepatitis reovirus (THRV)
-              </Radio>
-              <Radio value="All" onClick={this.strainHandler}>
-             All
-              </Radio>
-            </Radio.Group>
-          </div>
-          <Divider />
-        </div>
-        <div className="row flex-lg-row justify-content-center mt-3">
           <div className="col-md-6 mb-3">
             <Radio.Group name="radiogroup" defaultValue={"M2"}>
               <h5>Select Segment</h5>
               <Radio value="L1" onClick={this.segmentHandler}>
-                L1 
+                L1
               </Radio>
               <Radio value="L2" onClick={this.segmentHandler}>
                 L2
@@ -172,76 +242,140 @@ export default class Search extends React.Component {
               )}
             </Radio.Group>
           </div>
-
+          <Divider />
+        </div>
+        <div className="row flex-lg-row justify-content-center mt-3">
+          <div className="col-md-9 my-3">
+            <Radio.Group name="radiogroup" defaultValue={"TRV"}>
+              <h5>Select Variant Type</h5>
+              <Radio value="TRV" onClick={this.strainHandler}>
+                Turkey reovirus  (TRV)
+              </Radio>
+              <Radio value="TERV" onClick={this.strainHandler}>
+                Turkey enteric reovirus (TERV)
+              </Radio>
+              <Radio value="TARV" onClick={this.strainHandler}>
+                Turkey arthritis reovirus (TARV)
+              </Radio>
+              <Radio value="THRV" onClick={this.strainHandler}>
+                Turkey hepatitis reovirus (THRV)
+              </Radio>
+              <Radio value="All" onClick={this.strainHandler}>
+                All
+              </Radio>
+            </Radio.Group>
+          </div>
           <Divider />
         </div>
         <div className="row justify-content-center my-3">
-        <Radio.Group name="radiogroup" defaultValue={"phylo"}>
-              <h5>Select a Analysis to Perform</h5>
-              <Radio value="blast" onClick={this.analysisTypeHandler}>
-               Similarity Search
-              </Radio>
-              <Radio value="phylo" onClick={this.analysisTypeHandler}>
-               Phylogenetics Analysis
-              </Radio>
-            </Radio.Group>
-            
+          <Radio.Group name="radiogroup" defaultValue={"blast"}>
+            <h5>Select a Analysis to Perform</h5>
+            <Radio value="blast" onClick={this.analysisTypeHandler}>
+              Similarity Search
+            </Radio>
+            <Radio value="phylo" onClick={this.analysisTypeHandler}>
+              Phylogenetics Analysis
+            </Radio>
+          </Radio.Group>
         </div>
         <Divider />
-        {this.state.atype !=='blast' && (
-          <div className="row justify-content-center">
-        <div className="col-md-4 my-3">
-            <Radio.Group name="radiogroup" defaultValue={"mafft"}>
-              <h5>Select Multiple Alignment Tool</h5>
-              <Radio value="mafft" onClick={this.msaToolHandler}>
-               Mafft
-              </Radio>
-              <Radio value="muscle" onClick={this.msaToolHandler}>
-                Muscle
-              </Radio>
-              <Radio value="clustalw" onClick={this.msaToolHandler}>
-               ClustalW
-              </Radio>
-            </Radio.Group>
-          </div>
-          <div className="col-md-4 my-3">
-            <Radio.Group name="radiogroup" defaultValue={"ml"}>
-              <h5>Select Phylogeny Method</h5>
-              <Radio value="ml" onClick={this.phyloMethodHandler}>
-               Maximum likelihood
-              </Radio>
-              <Radio value="nj" onClick={this.phyloMethodHandler}>
-               Neighbor Joining
-              </Radio>
-            </Radio.Group>
-          </div>
-          <Divider />
+        {this.state.atype === 'blast' && (
+          <div className="row justify-content-center my-3">
+            <div className="col-md-3 my-3">
+              {this.state.seqType === 'nucl' && (
+                <Radio.Group name="radiogroup" defaultValue={"blastn"}>
+                  <h5>Select Blast Variant</h5>
+
+                  <Radio value="blastn" onClick={this.blastToolHandler}>
+                    blastn
+                  </Radio>
+                </Radio.Group>
+              )}
+              {this.state.seqType === 'prot' && (
+                <Radio.Group name="radiogroup" defaultValue={"blastp"}>
+                  <h5>Select Blast Variant</h5>
+                  <Radio value="blastp" onClick={this.blastToolHandler}>
+                    blastp
+                  </Radio>
+                  <Radio value="blastx" onClick={this.blastToolHandler}>
+                    blastx
+                  </Radio>
+                </Radio.Group>
+              )}
+
+
+
+
+            </div>
+            <div className="col-md-3">
+              <label className='h5'>Maximum Target Sequences</label>
+              <Slider defaultValue={5} marks={{ 1: '1', 50: '50' }} disabled={false} min={1} max={50} onChange={this.getTarget} />
+            </div>
+            <div className="col-md-3">
+              <label className='h5'>Word Size</label>
+              <Slider defaultValue={20} marks={{ 1: '1', 50: '50' }} disabled={false} min={1} max={50} onChange={this.getWord} />
+            </div>
+            <div className="col-md-3">
+              <label className='h5'>Exprected Threshold value</label>
+              <input className="form-control" type='text' placeholder="0.01" defaultValue={this.state.evalue} onChange={this.getEvalue}></input>
+            </div>
           </div>
         )}
-    
-        
-          <div className="row justify-content-center my-3">
-          <div className="col-md-5">
-              <h5>Enter Nucleotide or Protein FASTA Sequences here.</h5>
-              <Form.Control className="kbl-form mb-4" as="textarea" rows={4} placeholder={"Paste FASTA Sequence "} onChange={this.handleGeneChange}
-                value={this.state.genes}  spellCheck={false} />
-              <Button className="kbl-btn-1 mx-3" onClick={e => {
-                this.setState({ genes: geneSample });
-              }}>Sample Data</Button>
-              <Button className="kbl-btn-3" onClick={e => {
-                this.setState({ genes: "" })
-              }}>Clear Data</Button>
+        <Divider />
+        {this.state.atype !== 'blast' && (
+          <div className="row justify-content-center">
+            <div className="col-md-4 my-3">
+              <Radio.Group name="radiogroup" defaultValue={"mafft"}>
+                <h5>Select Multiple Alignment Tool</h5>
+                <Radio value="mafft" onClick={this.msaToolHandler}>
+                  Mafft
+                </Radio>
+                <Radio value="muscle" onClick={this.msaToolHandler}>
+                  Muscle
+                </Radio>
+                <Radio value="clustalw" onClick={this.msaToolHandler}>
+                  ClustalW
+                </Radio>
+              </Radio.Group>
             </div>
-            <div className="col-md-1 mt-5"><b>OR</b></div>
-                  <div className="col-md-3 mb-5">
-                    <h5 className="mt-5 pl-2"> Upload Sequence File</h5>
-
-                    <FileInput handler={this.fileSelected} />
-                  </div>
+            <div className="col-md-4 my-3">
+              <Radio.Group name="radiogroup" defaultValue={"ml"}>
+                <h5>Select Phylogeny Method</h5>
+                <Radio value="ml" onClick={this.phyloMethodHandler}>
+                  Maximum likelihood
+                </Radio>
+                <Radio value="nj" onClick={this.phyloMethodHandler}>
+                  Neighbor Joining
+                </Radio>
+              </Radio.Group>
+            </div>
+            <Divider />
           </div>
+        )}
 
-         <Divider />
-         <div className="row flex-lg-row justify-content-center g-2 my-5">
+
+        <div className="row justify-content-center my-3">
+          <div className="col-md-5">
+            <h5>Enter Nucleotide or Protein FASTA Sequences here.</h5>
+            <Form.Control className="kbl-form mb-4" as="textarea" rows={4} placeholder={"Paste FASTA Sequence "} onChange={this.handleSeqChange}
+              value={this.state.seqtxt} spellCheck={false} />
+            <Button className="kbl-btn-1 mx-3" onClick={e => {
+              this.setState({ seqtxt: geneSample });
+            }}>Sample Data</Button>
+            <Button className="kbl-btn-3" onClick={e => {
+              this.setState({ seqtxt: "" })
+            }}>Clear Data</Button>
+          </div>
+          <div className="col-md-1 mt-5"><b>OR</b></div>
+          <div className="col-md-3 mb-5">
+            <h5 className="mt-5 pl-2"> Upload Sequence File</h5>
+
+            <FileInput handler={this.fileSelected} />
+          </div>
+        </div>
+
+        <Divider />
+        <div className="row flex-lg-row justify-content-center g-2 my-5">
           {this.state.isOpen && (
 
             <div className="col-md-8">
@@ -259,7 +393,7 @@ export default class Search extends React.Component {
               <Button
                 className="kbl-btn-1  mx-3"
                 size="lg"
-                onClick={this.getPhylogeny}
+                onClick={this.runBlast}
               >
                 Run Analysis{" "}
               </Button>
